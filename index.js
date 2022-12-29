04f407eab9486b9347fd4392916d49049dca2d28
mariagul124 <96912779+mariagul124@users.noreply.github.com>
2022-12-29 10:09:12 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express') 
 const bodyParser = require('body-parser')
 const app = express()
-const port = 6000
+const port = 3000
 const TodoController = require('./controllers/todoController')
 const TODO_BASE_ROUTE = '/todo';
 const todoController = new TodoController()
